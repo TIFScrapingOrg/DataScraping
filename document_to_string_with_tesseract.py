286da312c91de00416b1ca66668ad8954e0b4f78
Noah Baxley <noah.baxley@gmail.com>
2024-02-29 12:40:02 -0600

    Example doc for finding text in pdf

---
+++ document_to_string_with_tesseract.py
@@ -0,0 +1,38 @@
+from PIL import Image
+import pytesseract
+import fitz
+import time
+import io
+
+start_time = time.time()
+
+# Our test pdf
+pdf_path = "PDF_transcribing_examples/T_003_43rdDamenAR98.pdf"
+
+
+with fitz.open(pdf_path) as pdf:
+	
+	for page_number in range(len(pdf)):
+
+		# We want to use an in-memory version of an image instead of
+		# going through the process of converting then saving it to the
+		# filesystem
+
+		# This section of the code uses PyMuPDF to turn each page into
+		# it's full resolution image form
+		page = pdf.load_page(page_number)
+		pixmap = page.get_pixmap(dpi=300)
+
+		image = Image.frombytes("RGB", [pixmap.width, pixmap.height], pixmap.samples)
+		image_bytes = io.BytesIO()
+
+		image.save(image_bytes, jpg_quality=100, format="PNG")
+		image_bytes.seek(0)
+
+		# Now use that image in tesseract
+		pdf_page_string = pytesseract.image_to_string(Image.open(image_bytes))
+		
+		# Print it out or do what ever
+		print(pdf_page_string)
+
+print(f'Time taken: {time.time - start_time} seconds')
