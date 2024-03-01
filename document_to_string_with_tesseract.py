72aa9e5dfb1de087a50a6b0e02aac04b55f48573
John Stock <jmks1000@gmail.com>
2024-03-01 12:33:15 -0600

    added basic regex check to find page numbers that have the end of year balances to them.

--- document_to_string_with_tesseract.py
+++ document_to_string_with_tesseract.py
@@ -3,6 +3,8 @@ import pytesseract
 import fitz
 import time
 import io
+import re
+import sys
 
 start_time = time.time()
 
@@ -11,7 +13,7 @@ pdf_path = "PDF_transcribing_examples/T_003_43rdDamenAR98.pdf"
 
 
 with fitz.open(pdf_path) as pdf:
-	
+
 	for page_number in range(len(pdf)):
 
 		# We want to use an in-memory version of an image instead of
@@ -22,7 +24,6 @@ with fitz.open(pdf_path) as pdf:
 		# it's full resolution image form
 		page = pdf.load_page(page_number)
 		pixmap = page.get_pixmap(dpi=300)
-
 		image = Image.frombytes("RGB", [pixmap.width, pixmap.height], pixmap.samples)
 		image_bytes = io.BytesIO()
 
@@ -32,7 +33,12 @@ with fitz.open(pdf_path) as pdf:
 		# Now use that image in tesseract
 		pdf_page_string = pytesseract.image_to_string(Image.open(image_bytes))
 		
-		# Print it out or do what ever
-		print(pdf_page_string)
+		financialsRegExTrigger = re.compile('statements of[ ]?(?:revenue|expenditure|change in fund balance)',re.IGNORECASE)
+
+		if financialsRegExTrigger.search(pdf_page_string):
+			print("-----" + str(page_number) + "-----")
+			print(pdf_page_string)
+			sys.exit()
+
 
 print(f'Time taken: {time.time - start_time} seconds')
