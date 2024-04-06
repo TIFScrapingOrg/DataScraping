21b22205c51f850c7d3f0d7bdf3a9fe354eb627f
Noah Baxley <noah.baxley@gmail.com>
2024-04-05 20:57:53 -0500

    Outline the column plan

--- table_parsing/handle_formats/process_report_test.py
+++ table_parsing/handle_formats/process_report_test.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import re
+import pprint
 
 DATABASE_FIELDS = ['year', 'tif_number', 'page_num', 'block_num', 'par_num', 'line_num', 'word_num', 'left', 'top', 'width', 'height', 'conf', 'text']
 
@@ -40,7 +41,6 @@ page = page.drop(USELESS_COLS, axis=1)
 
 # Iteratively join groups that have an average closer than 15px until there are no merges
 
-
 page.sort_values(['center_y'])
 
 line_dict = {}
@@ -77,6 +77,8 @@ while REDUCED:
 			del line_dict[current_keys[i]]
 			del line_dict[current_keys[i+1]]
 
+
+
 # Combine line items that are closer than 50px to each other on the horizontal
 # axis
 			
@@ -93,8 +95,6 @@ for key, words in line_dict.items():
 
 			distance = words[i+1]['left'] - (words[i]['width'] + words[i]['left'])
 			
-			# TODO: 40 is catching the words and gluing them together. I need to
-			# only grab stuff on the right half of the screen
 			if not REDUCED and distance < 40:
 
 				REDUCED = True
@@ -106,15 +106,14 @@ for key, words in line_dict.items():
 					'width': words[i]['width'] + distance + words[i+1]['width'],
 					'height': max(words[i]['top'] + words[i]['height'], words[i+1]['top'] + words[i+1]['height']) - top,
 					'conf': min(words[i]['conf'], words[i+1]['conf']),
-					'text': words[i]['text'] + '_' + words[i+1]['text']
+					# 'text': words[i]['text'] + '_' + words[i+1]['text']
+					'text': words[i]['text'] + words[i+1]['text']
 				}
 
 				words[i] = replacement
 				words.pop(i+1)
 
 				line_dict[key] = words
-				
-
 
 # Create a string representation of each line
 
@@ -129,4 +128,56 @@ for level in sorted(list(line_dict.keys())):
 	if len(line_dict) > 0:
 		document_line_dict[level] = document_line_dict[level][:-1]
 
-print(document_line_dict)
+print('List of lines')
+pprint.pp(document_line_dict)
+
+
+# Now try to extract columns
+# All of the numbers, at least, are right aligned in the columns
+# What this looks like is:
+#     1999
+# -----------
+#       5,555
+#      10,666
+#      64,111
+#         333
+#    (659,000)
+#   1,511,632
+
+# Now you'll notice that the parens () on the negative numbers don't follow this
+# rule so we won't catch everything like that
+
+# My solution is to compute two evenly spaced out lines spanning the entire
+# width of the caught numbers and then putting any element that crosses over
+# either of those lines inside of the column:
+#     |    |
+#     1999
+# -----------
+#     |    |
+#       5,555
+#      10,666
+#      64,111
+#     |    |
+#         333
+#    (659,000)
+#   1,511,632
+#     |    |
+# The drawback is that this may catch words above or below the columns.
+# The solution? We know where the top and bottom rows of the table should be.
+# They will always be either "Revenues" or have "end of year" in them.
+#                                 |    |
+#                                 1999
+#  Revenues                   
+#    Property tax                   7,455
+#    Sales tax                      5,555
+#    Interest                      10,666
+#
+#  Expenditures                    64,111
+#                                 |    |
+#  ...                                333
+#                                (659,000)
+#  Fund balance, end of year    1,511,632
+#                                 |    |
+# This will cut off the column headers though. My one guess as to how to fix
+# that is to just draw the column of numbers up until there is a gap of at least
+# 50 px. It's probably not fool proof, but it will work
